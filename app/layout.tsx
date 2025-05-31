4ca04307eb473200a4500152103b1844c9e051f4
ItsMe Prince <mohammadbadshah92@gmail.com>
2025-05-31 20:47:22 +0530

    1.0.1 | Project initialized

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Suspense } from "react";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prince-kun | Showcase",
+  description: ".",
+  icons: {
+    icon: "/public",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
+      </body>
+    </html>
+  );
+}
